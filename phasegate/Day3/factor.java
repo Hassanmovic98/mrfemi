abae2c4dfe5598612d9e99997263f1f78979a1d3
Hassanmovic98 <salakohassanmobolaji@gmail.com>
2025-05-29 17:34:42 +0100

    'git'

--- phasegate/Day3/factor.java
+++ phasegate/Day3/factor.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
-public class factor {
+public class Factor {
 	public static void main (String [] args) {
-
 
 	Scanner input = new Scanner(System.in); 
 
-	System.out.print ("enter a number");
-
+	System.out.print ("enter a number: ");
 	int number = input.nextInt();
 
-	 for (int i = 0; i <= number; i++) {
-		if (number % i == 0);
-	   System.out.print("i");
+	 for(int i = 1; i <= number; i++){
+		if(number % i == 0){
+		System.out.print(i);
 	 }
-	
+}
 
      }	
 
